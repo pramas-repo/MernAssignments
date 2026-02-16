2b721256bafe6f29c9483d46a7a229c9c9aca26e
pramas-repo <shrikantpande2000@gmail.com>
2026-02-16 13:37:10 +0530

    My First Commit

---
+++ a1.js
@@ -0,0 +1,4 @@
+/* my first js program */
+
+var name = "Rajesh Gupta"
+console.log("Hello and welcome " + name)
---
+++ a2.js
@@ -0,0 +1,6 @@
+/* area of triangle */
+
+var ht = 12
+var ba = 8
+var area = (ht * ba) / 2
+console.log("Area of triangle is " + area)
